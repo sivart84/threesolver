99301a873d16ca8c8e7b9e6458770d8d33158c2a
sivart84 <git@sivart.cc>
2014-04-05 15:54:44 -0700

    Move game logic into a class.

--- game.rb
+++ game.rb
@@ -1,24 +1,38 @@
 require_relative 'lib/board'
 require_relative 'lib/stack'
 
-def display(stack, board)
-  puts "NEXT CARD: #{stack.next?}"
-  board.translate.each do |row|
-    print "#{row}\n"
+class Game
+
+  attr_accessor :game_board, :rng, :stack, :seed
+
+  def initialize(seed = rand(2048))
+    @seed = seed
+    @rng = Random.new(@seed)
+    puts "USING SEED: #{@seed}\n\n"
+    @stack = Stack.new(@rng)
+    @game_board = Board.new(@rng, @stack)
+    run
   end
+
+  def display
+    puts "NEXT CARD: #{@stack.next?}"
+    @game_board.translate.each do |row|
+      print "#{row}\n"
+    end
+  end
+
+  def run
+    loop do
+      display
+      puts "\n"
+      puts "Move: "
+      dir = gets.chomp.to_sym
+      @game_board.move(dir)
+    end
+  end
+
 end
 
 begin
-  seed = rand(2048)
-  rng = Random.new(seed)
-  puts "USING SEED: #{rng.seed}\n\n"
-  stack = Stack.new(rng)
-  game_board = Board.new(rng, stack)
-  loop do
-    display(stack, game_board)
-    puts "\n"
-    puts "Move: "
-    dir = gets.chomp.to_sym
-    game_board.move(dir)
-  end
+  Game.new
 end
